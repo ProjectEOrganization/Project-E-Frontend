33606492b5c28336fd7aa5e06450f6d75f2b2e24
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-13 20:41:25 +0200

    load all assets on app start

--- ProjectE/hooks/useCachedResources.ts
+++ ProjectE/hooks/useCachedResources.ts
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
@@ -15,7 +15,12 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
+          ...FontAwesome5.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'Inter-Medium': require('../assets/fonts/Inter/Inter-Medium.ttf'),
+          'Inter-Bold': require('../assets/fonts/Inter/Inter-Bold.ttf'),
+          'Inter-Regular': require('../assets/fonts/Inter/Inter-Regular.ttf'),
+          'Inter-SemiBold': require('../assets/fonts/Inter/Inter-SemiBold.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
