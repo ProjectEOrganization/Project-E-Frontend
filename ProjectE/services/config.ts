19f9f64bf6a4717dc867200f5389aacc4d22ff4f
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-14 20:50:08 +0200

    Update config.ts

--- ProjectE/services/config.ts
+++ ProjectE/services/config.ts
@@ -1,6 +1,6 @@
 export default {
-    API_URL: 'http://157.230.68.213',
-    SOCKET_URL: 'ws://157.230.68.213',
+    API_URL: 'https://api.rapidapp.live/',
+    SOCKET_URL: 'wss://api.rapidapp.live/',
     firebase: {
         apiKey: "AIzaSyA-H3h0czlXG_9THrjbmTpTnZwxsbHSAK0",
         authDomain: "projecte-cc03f.firebaseapp.com",
