65778d34c48d0334b1477714b37750cc94d3d34c
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-14 15:57:13 +0200

    api url

--- ProjectE/services/config.ts
+++ ProjectE/services/config.ts
@@ -1,6 +1,6 @@
 export default {
-    API_URL: '',
-    SOCKET_URL: '',
+    API_URL: 'http://157.230.68.213',
+    SOCKET_URL: 'http://157.230.68.213',
     firebase: {
         apiKey: "AIzaSyA-H3h0czlXG_9THrjbmTpTnZwxsbHSAK0",
         authDomain: "projecte-cc03f.firebaseapp.com",
