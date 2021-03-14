eae6fcaf067ecc296970b1d3b33b4f134302255a
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-14 15:59:15 +0200

    add socket url

--- ProjectE/services/config.ts
+++ ProjectE/services/config.ts
@@ -1,6 +1,6 @@
 export default {
     API_URL: 'http://157.230.68.213',
-    SOCKET_URL: 'http://157.230.68.213',
+    SOCKET_URL: 'ws://157.230.68.213',
     firebase: {
         apiKey: "AIzaSyA-H3h0czlXG_9THrjbmTpTnZwxsbHSAK0",
         authDomain: "projecte-cc03f.firebaseapp.com",
