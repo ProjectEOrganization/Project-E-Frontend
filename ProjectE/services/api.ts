8b112cac1bf1e91bf73412ea87492cd6b4ecaab1
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-15 13:07:40 +0200

    automatic token refresh

--- ProjectE/services/api.ts
+++ ProjectE/services/api.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
 })
 
 api.interceptors.request.use(async (config: AxiosRequestConfig) => {
-    const token = await firebase.auth().currentUser?.getIdToken();
+    const token = await firebase.auth().currentUser?.getIdToken(true);
     config.headers['authorization'] = token
     config.headers['DEV-INFO'] = `appVer=${Constants.nativeAppVersion}, iosVer=${Device.osVersion}, device=${Device.modelId}, GMT=${new Date().getTimezoneOffset() / 60}`;
     return config
