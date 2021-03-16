67113af6f41649dbec33b2b35f1a314a08ffd662
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-16 19:29:58 +0200

    close modal after clicking outside

--- ProjectE/components/Modals/LoginModal.tsx
+++ ProjectE/components/Modals/LoginModal.tsx
@@ -1,12 +1,18 @@
 import { useNavigation } from '@react-navigation/core'
 import React from 'react'
-import { View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import Login from '../Auth/Login'
+const { width, height } = Dimensions.get('screen');
 
 export default function LoginModal() {
+    const navigation = useNavigation();
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Login />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ zIndex: 999 }}>
+                <Login />
+            </View>
+            <View onTouchStart={navigation.goBack} style={{ width, height, position: 'absolute' }} />
         </View>
     )
 }
--- ProjectE/components/Modals/RegisterModal.tsx
+++ ProjectE/components/Modals/RegisterModal.tsx
@@ -1,11 +1,17 @@
+import { useNavigation } from '@react-navigation/core'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions } from 'react-native'
 import Register from '../Auth/Register'
+const { width, height } = Dimensions.get('screen');
 
 export default function RegisterModal() {
+    const navigation = useNavigation();
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Register />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ zIndex: 999 }}>
+                <Register />
+            </View>
+            <View onTouchStart={navigation.goBack} style={{ width, height, position: 'absolute' }} />
         </View>
     )
 }
