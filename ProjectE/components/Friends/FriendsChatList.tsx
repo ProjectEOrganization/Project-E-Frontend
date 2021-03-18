d9b4fa8dfae5a2ab86c02dea64632418da1426b2
Martynas Alekna <martynas.alekna10@gmail.com>
2021-03-18 13:31:50 +0200

    load chats with friends

--- ProjectE/components/Friends/FriendsChatList.tsx
+++ ProjectE/components/Friends/FriendsChatList.tsx
@@ -1,18 +1,43 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import { api } from '../../services/api'
 import { useAuth } from '../../services/auth'
 import FriendsMessagesCard from './FriendsMessagesCard'
 
 export default function FriendsChatList() {
     const [chats, setChats] = useState([])
+    const [loading, setLoading] = useState(false)
     const auth = useAuth();
 
     useEffect(() => {
-        api.get('/chats').then((res) => {
-            setChats(res.data)
-        })
+        setLoading(true);
+        api.get('/chats')
+            .then((res) => {
+                setChats(res.data)
+                setLoading(false)
+            })
+            .catch(() => {
+                setChats([])
+                setLoading(false)
+            })
+
     }, [auth])
+
+    if (loading) {
+        return (
+            <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <ActivityIndicator size="small" color="#4B00FF" />
+            </View>
+        )
+    }
+
+    if (chats.length === 0) {
+        return (
+            <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>No chats</Text>
+            </View>
+        )
+    }
 
     return (
         <View>
