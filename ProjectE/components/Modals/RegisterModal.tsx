97a232eaa30129343ce4175ac2799f51c488cfa8
Martynas Alekna <martynas.alekna10@gmail.com>
2021-04-20 11:40:42 +0300

    friend request improvements

--- ProjectE/components/Modals/RegisterModal.tsx
+++ ProjectE/components/Modals/RegisterModal.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from '@react-navigation/core'
+import { useNavigation, useRoute } from '@react-navigation/core'
 import React from 'react'
 import { View, Text, Dimensions } from 'react-native'
 import Register from '../Auth/Register'
@@ -6,10 +6,11 @@ const { width, height } = Dimensions.get('screen');
 
 export default function RegisterModal() {
     const navigation = useNavigation();
+    const route = useRoute();
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ zIndex: 999 }}>
-                <Register />
+                <Register actionAfter={route.params?.actionAfter} />
             </View>
             <View onTouchStart={navigation.goBack} style={{ width, height, position: 'absolute' }} />
         </View>
