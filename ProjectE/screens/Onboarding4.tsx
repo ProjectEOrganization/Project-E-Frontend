1d8e7bf4124dd9b7187b25f8f333c440225cb306
Alen Niu <43788415+alenniu@users.noreply.github.com>
2021-12-08 03:54:49 -0800

    small fix

--- ProjectE/screens/Onboarding4.tsx
+++ ProjectE/screens/Onboarding4.tsx
@@ -1,367 +1,187 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, TouchableOpacity, Image, Alert } from "react-native";
+import React, { useState, useEffect, useRef, Component } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  FlatList,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
-import { useFonts } from "expo-font";
-import SvgComponent from "../assets/discard/svgComponent.js";
-import SvgComponent1 from "../assets/discard/svgComponent1.js";
-import OnBoardingSvgComponent3 from "../assets/onBoardingSvgComponent3.js";
+
 import Navigation from "../navigation";
-import { LinearGradient } from "expo-linear-gradient";
 
 import { api } from "../services/api";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import TabOneScreen from "./FriendsScreen";
 import { useAuth } from "../services/auth";
-import { createIconSetFromFontello } from "@expo/vector-icons";
 
-// import * as yourModuleName from 'module-name';
+import tw from "twrnc";
+
+const { width } = Dimensions.get("window");
 
 export default function TabThreeScreen() {
   const auth = useAuth();
-  const { params } = useRoute();
-  async function anonymousSignin() {
-    await auth.signInAnonymously();
-    var user = auth.user;
-    await user
-      .updateProfile({
-        displayName: randomName,
-        photoURL: photoURL,
-      })
-      .then((data) => {
-        console.log(data);
-      });
-    navigation.navigate("RandomChat");
-  }
 
-  async function getRandomName() {
-    const newRandomName = await (
-      await api.get("/generate_display_name")
-    ).data.name;
-    // const avatars = await (await api.get("avatars")).data.avatars;
-    // console.log("avatars: ", avatars);
-    setRandomName(newRandomName);
-    console.log(newRandomName);
-    console.log(photoURL);
-  }
-  function register() {
-    navigation.navigate("Friends");
-
-    // auth
-    //   .signupWithPhoto(
-    //     params.email,
-    //     params.password,
-    //     params.displayName,
-    //     photoURL
-    //   )
-    //   // console.log(params)
-    //   .then(() => {
-    //     navigation.navigate("Friends");
-    //   })
-    //   .catch(() => {
-    //     setSuccess(false);
-    //   });
-  }
   const navigation = useNavigation();
 
-  const [randomName, setRandomName] = useState("Green Chinchilla");
-  const [success, setSuccess] = useState(true);
-  const [photoURL, setPhotoURL] = useState(
-    "https://rapid.nyc3.digitaloceanspaces.com/avatars/01-dog.png"
-  );
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     ScrollView.scrollTo(x: 30)
+  // });
+
+  const onNext = async () => {
+    if (true) {
+      navigation.navigate("RandomChat");
+    } else {
+      // setSuccess(false)
+    }
+  };
+
+  // this.scrollView.scrollTo({ x: -30 });
 
   return (
-    <View style={styles.container}>
-      {/* <Text style={styles.title2}>Rapid</Text> */}
-      {/* <SvgComponent /> */}
+    <View
+      style={[
+        tw`flex-1 items-center justify-start `,
+        { backgroundColor: "#EFEEFC" },
+      ]}
+    >
+      <Image
+        style={{ height: "100%", width: "100%" }}
+        source={require("../assets/images/pageback4.png")}
+      />
 
-      <OnBoardingSvgComponent3 />
-      {/* <Text style={styles.title1}>ONBOARDING 2 {"\n"}TO RAPID</Text> */}
+      <Image
+        style={{ height: 100, width: 100, marginTop: 40, position: "absolute" }}
+        source={require("../assets/images/bluetextlogo.png")}
+      />
 
-      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
-      {/* <EditScreenInfo path="/screens/TabTwoScreen.tsx" /> */}
-
-      <Text style={styles.title}>Choose your Profile Pic</Text>
       <View
         style={{
+          backgroundColor: "white",
+          height: 576,
+          width: 362,
+          borderRadius: 35,
           position: "absolute",
-          top: 1260,
-          left: 120,
-          borderRadius: 100,
-          borderWidth: 5,
-          borderColor: "#4B6EF6",
+          top: 220,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "start",
         }}
       >
-        <Image
-          style={{
-            margin: 20,
-            position: "relative",
-            top: -5,
-            left: -3,
-            width: 120,
-            height: 120,
-            padding: 0,
-          }}
-          source={
-            photoURL
-              ? { uri: photoURL }
-              : require("../assets/images/Profile-Male-PNG.png")
-          }
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.yesButton}
-        onPress={() =>
-          navigation.navigate("ChooseProfileModal", {
-            changeProfilePicFunc: setPhotoURL,
-          })
-        }
-      >
-        <Text style={styles.loginText}>Change</Text>
-      </TouchableOpacity>
-
-      {!success && (
         <Text
-          style={{
-            fontSize: 14,
-            fontFamily: "Inter-Bold",
-            color: "red",
-            textAlign: "center",
-            lineHeight: 23,
-            position: "absolute",
-            top: 1650,
-            left: 40,
-          }}
+          style={[
+            styles.textStyle,
+            {
+              color: "#009B70",
+              fontSize: 24,
+              fontFamily: "Rubik-Bold",
+              marginTop: 27,
+            },
+          ]}
         >
-          An account with this email has been created.
+          Let's get creative!
         </Text>
-      )}
 
-      {!success && (
-        <TouchableOpacity
-          style={styles.yesButton4}
-          onPress={() =>
-            navigation.navigate("ChooseProfileModal", {
-              changeProfilePicFunc: setPhotoURL,
-            })
-          }
+        <Text
+          style={[
+            styles.textStyle,
+            {
+              color: "#6C79FF",
+              fontSize: 22,
+              fontFamily: "Rubik-Medium",
+              marginTop: 30,
+            },
+          ]}
         >
-          <Text style={styles.loginText}>Go Back</Text>
+          Create your avatar
+        </Text>
+
+        <Text
+          style={[
+            styles.textStyle,
+            {
+              color: "#868595",
+              fontSize: 15,
+              fontFamily: "Rubik-Regular",
+              marginTop: 20,
+              lineHeight: 21,
+            },
+          ]}
+        >
+          Collect more avatars and cosmetics {"\n"}by playing more, making new
+          friends{"\n"}and more
+        </Text>
+        <View style={{ height: 200, marginTop: 30 }}>
+          <ScrollView
+            horizontal={true}
+            decelerationRate={0}
+            snapToInterval={width - 60}
+            snapToAlignment={"center"}
+            contentInset={{ top: 0, left: 30, bottom: 0, right: 30 }}
+            contentOffset={{ x: 300, y: 0 }}
+            contentContainerStyle={{ flexGrow: 1, height: 50 }}
+          >
+            <View style={styles.first} />
+            <View style={styles.second} />
+            <View style={styles.third} />
+          </ScrollView>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => onNext()}
+          style={[
+            tw`bg-blue-300 w-80 h-15 items-center justify-center`,
+            {
+              backgroundColor: "#4B6EF6",
+              borderRadius: "20",
+              marginTop: "auto",
+              marginBottom: 20,
+            },
+          ]}
+        >
+          <Text
+            style={{
+              fontFamily: "Rubik-Medium",
+              fontSize: 20,
+              color: "#FFFFFF",
+            }}
+          >
+            I'm ready!!
+          </Text>
         </TouchableOpacity>
-      )}
-      {/* 
-        <View 
-        style={{position: 'absolute',
-        top: 1380, 
-        left: 0,
-       
-        width: 400}}>
-
-          <Text style={styles.title3}>
-            We'll call you
-          </Text>
-
-          <Text style={styles.title4}>
-            {randomName}
-          </Text>
-          </View>
-
-          <TouchableOpacity style={styles.yesButton2} onPress={getRandomName} >
-            <Text style={styles.loginText2}>
-              Randomize
-          </Text>
-          </TouchableOpacity> */}
-
-      <LinearGradient
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-        colors={["#B3ECAE", "#00DBD0"]}
-        style={styles.yesButton3}
-      >
-        <TouchableOpacity onPress={() => register()}>
-          <Text style={styles.loginText3}>Let's Go!</Text>
-        </TouchableOpacity>
-      </LinearGradient>
-
-      <Text style={styles.title5}>
-        By pressing on “Ready!”, you agree to our Terms of Service and {"\n"}{" "}
-        acknowledge that you read our Privacy Policy
-      </Text>
-
-      {/* <View style={styles.container2}>
-        <Text style={styles.title3}>
-          Chat with random people and create
-          everlasting friendships,
-        <Text style={{ fontWeight: 'bold' }}> click below </Text>
-
-        </Text >
       </View>
-
-      <TouchableOpacity onPress={anonymousSignin}>
-        <SvgComponent1 />
-      </TouchableOpacity> */}
-
-      {/* <View style={styles.container3}>
-        <Text style={styles.title4}>
-          Already have an account?
-        <Text onPress={() => navigation.navigate('LoginModal')} style={{ fontFamily: 'Inter-SemiBold', color: '#4B00FF' }}> Log in </Text> */}
-      {/* <Text onPress={anonymousSignin} style={{fontFamily: 'Inter-SemiBold', color: '#4B00FF'}}> Log in as a guest</Text> */}
-      {/* </Text >
-      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // overall container
-    flex: 1,
-    alignItems: "center",
-    marginTop: -950,
-    paddingLeft: 80,
-    backgroundColor: "#FFFFFF",
-  },
-  title: {
-    position: "absolute",
-    top: 1180,
-    left: 40,
-    alignSelf: "center",
-    fontSize: 25,
-    fontWeight: "bold",
-    fontFamily: "Inter-ExtraBold",
-    color: "#4957FF",
-    letterSpacing: 1,
-  },
-  title1: {
-    //Welcome to
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "right",
-    fontFamily: "Inter-ExtraBold",
-    color: "#4957FF",
-    letterSpacing: 1,
-    position: "absolute",
-    top: 1100,
-    left: 20,
-  },
-  title2: {
-    //Welcome to
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "right",
-    fontFamily: "Inter-ExtraBold",
-    color: "#4957FF",
-    letterSpacing: 1,
-    position: "absolute",
-    top: 1140,
-    left: 110,
-  },
-  title3: {
-    //Welcome to
-    fontSize: 22,
+  textStyle: {
+    fontFamily: "Rubik-Medium",
     textAlign: "center",
-    fontFamily: "Inter-SemiBold",
-    color: "#6E6E6E",
-    letterSpacing: 0,
-    // position: 'absolute',
-    // top: 1380,
-    // left: 125
   },
-  title4: {
-    //Welcome to
-    fontSize: 28,
-    textAlign: "center",
-    fontFamily: "Inter-ExtraBold",
-    color: "#689CF6",
-    letterSpacing: 0,
-    marginTop: 10,
-    // position: 'absolute',
-    // top: 1420,
-    // left: 0
-  },
-  yesButton: {
-    backgroundColor: "#4957FF",
-    borderRadius: 15,
-    height: 60,
-    shadowOffset: { width: 2, height: 6 },
-    shadowColor: "#4957FF",
-    shadowOpacity: 0.27,
-    justifyContent: "center",
-    width: 190,
-    position: "absolute",
-    top: 1450,
-    left: 115,
-  },
-  yesButton4: {
-    backgroundColor: "#4957FF",
-    borderRadius: 15,
-    height: 40,
-    shadowOffset: { width: 2, height: 6 },
-    shadowColor: "#4957FF",
-    shadowOpacity: 0.27,
-    justifyContent: "center",
-    width: 140,
-    position: "absolute",
-    top: 1680,
-    left: 125,
-  },
-
-  loginText: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 25,
-    fontFamily: "Inter-ExtraBold",
-  },
-  yesButton2: {
-    borderColor: "#4957FF",
-    borderWidth: 3,
+  first: {
+    backgroundColor: "#FFCCEF",
+    width: width - 80,
+    margin: 10,
+    height: 170,
     borderRadius: 10,
-    height: 35,
-    shadowOffset: { width: 2, height: 6 },
-    shadowColor: "#4957FF",
-    shadowOpacity: 0.27,
-    justifyContent: "center",
-    width: 140,
-    position: "absolute",
-    top: 1465,
-    left: 130,
   },
-  loginText2: {
-    color: "#4957FF",
-    textAlign: "center",
-    fontSize: 16,
-    fontFamily: "Inter-ExtraBold",
+  second: {
+    backgroundColor: "#CCE4FF",
+    width: width - 80,
+    margin: 10,
+    height: 170,
+    borderRadius: 10,
   },
-
-  yesButton3: {
-    backgroundColor: "#50E3C2",
-    borderRadius: 25,
-    height: 70,
-    shadowOffset: { width: 2, height: 6 },
-    shadowColor: "#50E3C2",
-    shadowOpacity: 0.27,
-    justifyContent: "center",
-    width: 325,
-    position: "absolute",
-    top: 1565,
-    left: 50,
-  },
-  loginText3: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 25,
-    fontFamily: "Inter-ExtraBold",
-  },
-  title5: {
-    fontSize: 10,
-    textAlign: "center",
-    fontFamily: "Inter-Medium",
-    color: "#A9ACB0",
-    letterSpacing: 0,
-    marginTop: 10,
-    position: "absolute",
-    top: 1720,
-    left: 55,
+  third: {
+    backgroundColor: "#FFFDCC",
+    width: width - 80,
+    margin: 10,
+    height: 170,
+    borderRadius: 10,
   },
 });
